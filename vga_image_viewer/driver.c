098f6faee52055e7084e388f818abec7d23f767e
Sem van der Hoeven <semmer99@gmail.com>
2022-12-21 14:48:21 +0100

    add linux code

---
+++ vga_image_viewer/driver.c
@@ -0,0 +1,87 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/platform_device.h>
+#include <linux/sched.h>
+#include <linux/io.h>
+#include <linux/of.h>
+
+MODULE_LICENSE("GPL");
+
+#define HW_REGS_BASE 0xff200000
+#define HW_REGS_SPAN 0x00200000
+#define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
+#define PIXEL_DATA_BASE 0x000000030
+#define PIXEL_STATUS_READ_BASE 0x000000010
+#define PIXEL_STATUS_WRITE_BASE 0x000000020
+#define PIXEL_ROW_BASE 0x000000000
+
+#define DEVNAME "Mijn Module"
+
+void * LW_virtual; // Lightweight bridge base address
+volatile int *PIXEL_base_ptr; // virtual addresses
+volatile int *PIXEL_status_r_ptr;
+volatile int *PIXEL_status_w_ptr;
+volatile int *PIXEL_row_ptr;
+
+irq_handler_t irq_handler (int irq, void *dev_id, struct pt_regs * regs)
+{
+        printk(KERN_ALERT "In de IRQqqqqqqqqqqqqqqqqqqqqq!");
+        return (irq_handler_t) IRQ_HANDLED;
+}
+
+static int init_handler(struct platform_device * pdev)
+{
+        int irq_num,ret;
+
+        //Koppel fysiek geheugenbereik aan pointer
+        LW_virtual = ioremap(HW_REGS_BASE, HW_REGS_SPAN);
+
+        PIXEL_status_r_ptr = LW_virtual + PIXEL_STATUS_READ_BASE; //offset naar PIO registers
+	PIXEL_status_w_ptr = LW_virtual + PIXEL_STATUS_WRITE_BASE;
+
+	*(PIXEL_status_r_ptr + 2) = 0xF; // enable irq interrupts
+
+        irq_num = platform_get_irq(pdev,0);
+        printk(KERN_ALERT DEVNAME ": IRQ %d wordt geregistreert!\n", irq_num);
+
+        ret = request_irq(irq_num, (irq_handler_t) irq_handler, 0, DEVNAME, NULL);
+
+        return ret;
+}
+static int  clean_handler(struct platform_device *pdev)
+{
+        int irq_num;
+        irq_num=platform_get_irq(pdev,0);
+        printk(KERN_ALERT DEVNAME ": IRQ %d wordt vrijgegeven!\n", irq_num);
+
+        iounmap (LW_virtual); //mapping ongedaan maken
+        free_irq(irq_num, NULL);
+        return 0;
+}
+
+
+/*
+* Hierin beschrijven we welk device we willen koppelen
+* aan onze module. Deze moet in de device-tree overeen-
+* komen!
+*/
+static const struct of_device_id mijn_module_id[] ={
+        {.compatible = "altr,pixel_status_read"},
+        {}
+};
+
+//handlers e.d. koppelen
+static struct platform_driver mijn_module_driver = {
+        .driver = {
+                .name = DEVNAME,
+                .owner = THIS_MODULE,
+                .of_match_table = of_match_ptr(mijn_module_id),
+        },
+        .probe = init_handler,
+        .remove = clean_handler
+};
+
+//module registreren
+module_platform_driver(mijn_module_driver);
---
+++ vga_image_viewer/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#define HW_REGS_BASE ( 0xff200000 )
+#define HW_REGS_SPAN ( 0x00200000 )
+#define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
+
+#define PIXEL_DATA_BASE ( 0x100000030 )
+#define PIXEL_STATUS_WRITE_BASE (0x100000020)
+#define PIXEL_STATUS_READ_BASE ( 0x100000010 )
+#define PIXEL_ROW_BASE ( 0x100000000  )
+
+#define HWREG(x) (*((volatile uint32_t *)(x)))
+
+int main(void) {
+
+        volatile unsigned int *h2p_pixel_addr=NULL;
+        void *virtual_base;
+        int fd;
+
+        // Open /dev/mem
+        if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
+                        printf( "ERROR: could not open \"/dev/mem\"...\n" );
+                        return( 1 );
+        }
+
+        // Hiermee koppelen we het fysieke geheugen aan het virtuele geheugen
+        // en krijgen we de pointer terug
+        virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ),
+                                                MAP_SHARED, fd, HW_REGS_BASE );
+        if( virtual_base == MAP_FAILED ) {
+                printf( "ERROR: mmap() failed...\n" );
+                close( fd );
+                return(1);
+        }
+
+	for (int i = 0; i < 0xFFFFFF; i++) {
+		HWREG(virtual_base + PIXEL_DATA_BASE) = i;
+
+//		printf("%d\n",HWREG(virtual_base + PIXEL_STATUS_READ_BASE));
+	}
+
+
+	if( munmap( virtual_base, HW_REGS_SPAN ) != 0 ) {
+                printf( "ERROR: munmap() failed...\n" );
+                close( fd );
+                return( 1 );
+        }
+
+        close( fd );
+	return 0;
+}
