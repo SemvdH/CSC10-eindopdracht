4833d4d8cab465ce00abb3e3710e781dd56289bd
Sem van der Hoeven <semmer99@gmail.com>
2023-01-10 12:59:14 +0100

    correct types

--- vga_image_viewer/readbmp/readbmp.h
+++ vga_image_viewer/readbmp/readbmp.h
@@ -6,11 +6,11 @@
 
 typedef struct tagBITMAPFILEHEADER
 {
-    uint16_t bfType;  //specifies the file type
-    uint32_t bfSize;  //specifies the size in bytes of the bitmap file
-    uint16_t bfReserved1;  //reserved; must be 0
-    uint16_t bfReserved2;  //reserved; must be 0
-    uint32_t bfOffBits;  //specifies the offset in bytes from the bitmapfileheader to the bitmap bits
+    short bfType;  //specifies the file type
+    int bfSize;  //specifies the size in bytes of the bitmap file
+    short bfReserved1;  //reserved; must be 0
+    short bfReserved2;  //reserved; must be 0
+    int bfOffBits;  //specifies the offset in bytes from the bitmapfileheader to the bitmap bits
 }BITMAPFILEHEADER;
 
 #pragma pack(pop)
@@ -20,17 +20,17 @@ typedef struct tagBITMAPFILEHEADER
 
 typedef struct tagBITMAPINFOHEADER
 {
-    uint32_t biSize;  //specifies the number of bytes required by the struct
+    int biSize;  //specifies the number of bytes required by the struct
     long biWidth;  //specifies width in pixels
     long biHeight;  //specifies height in pixels
-    uint16_t biPlanes;  //specifies the number of color planes, must be 1
-    uint16_t biBitCount;  //specifies the number of bits per pixel
-    uint32_t biCompression;  //specifies the type of compression
-    uint32_t biSizeImage;  //size of image in bytes
+    short biPlanes;  //specifies the number of color planes, must be 1
+    short biBitCount;  //specifies the number of bits per pixel
+    int biCompression;  //specifies the type of compression
+    int biSizeImage;  //size of image in bytes
     long biXPelsPerMeter;  //number of pixels per meter in x axis
     long biYPelsPerMeter;  //number of pixels per meter in y axis
-    uint32_t biClrUsed;  //number of colors used by the bitmap
-    uint32_t biClrImportant;  //number of colors that are important
+    int biClrUsed;  //number of colors used by the bitmap
+    int biClrImportant;  //number of colors that are important
 }BITMAPINFOHEADER;
 
 #pragma pack(pop)
