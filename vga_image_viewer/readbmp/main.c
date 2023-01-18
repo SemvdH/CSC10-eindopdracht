75048834a1fd53314a3d3f60f248a86102854b75
SemvdH <semmer99@gmail.com>
2023-01-18 16:49:41 +0100

    add adding 0 to fourth byte of every pixel

--- vga_image_viewer/readbmp/main.c
+++ vga_image_viewer/readbmp/main.c
@@ -10,6 +10,15 @@ void print_raw_bmp(char* imgdata) {
 	}
 }
 
+void pad_with_zeros(char* imgdata, char* newimgdata) {
+	for (int i = 0; i < sizeof(imgdata)/sizeof(imgdata[0]); i+= 4) {
+		newimgdata[i] = imgdata[i];
+		newimgdata[i+1] = imgdata[i+1];
+		newimgdata[i+2] = imgdata[i+2];
+		newimgdata[i+3] = 0;
+	}
+}
+
 int main()
 {
 	BITMAPINFOHEADER bitmapInfoHeader;
@@ -21,9 +30,14 @@ int main()
 	{
 		printf("Shit");
 	}
+	unsigned char *paddedImage = (unsigned char*) malloc(480*640*4);
+
+	pad_with_zeros(bitmapData,paddedImage);
+
+	free(bitmapData);
 
 
-	printf("Image data:\n");
+	//printf("Image data:\n");
 
 	print_raw_bmp(bitmapData);
 	return 0;
